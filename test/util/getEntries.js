492df18b0afac0640e2bb7799a44e2c0ae11db9c
Martin Heidegger <martin.heidegger@gmail.com>
2018-02-08 04:03:31 +0900

    removed async await for backwards compatible tests

--- test/util/getEntries.js
+++ test/util/getEntries.js
@@ -2,8 +2,9 @@ const streamToArray = require('stream-to-array')
 const ReaddirStream = require('../..')
 const entryToString = require('./entryToString')
 
-async function getEntries (archive, opts) {
-  return (await streamToArray(new ReaddirStream(archive, opts))).map(entryToString)
+function getEntries (archive, opts) {
+  return streamToArray(new ReaddirStream(archive, opts))
+    .then(entries => entries.map(entryToString))
 }
 
 module.exports = getEntries
