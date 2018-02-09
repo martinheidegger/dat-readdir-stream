7a7e79363a9206a4bb3f063c581e291b49f8add6
Martin Heidegger <martin.heidegger@gmail.com>
2018-02-10 06:02:37 +0900

    perf: even quicker archive creation for tests

--- test/util/createArchive.js
+++ test/util/createArchive.js
@@ -3,17 +3,24 @@ const hyperdrive = require('hyperdrive')
 const ram = require('random-access-memory')
 const bb = require('bluebird')
 
+function isFolder (name) {
+  return /\/$/.test(name)
+}
+
 module.exports = names => {
   names = names || []
   const archive = hyperdrive(ram)
+  const mkdir = (name) => new Promise((resolve, reject) =>
+    archive.mkdir(name, err => err ? reject(err) : resolve())
+  )
+  const writeFile = (name) => new Promise((resolve, reject) => {
+    archive.writeFile(name, 'content', err => err ? reject(err) : resolve())
+  })
 
-  return bb.mapSeries(names, name =>
-    new Promise((resolve, reject) => {
-      if (/\/$/.test(name)) {
-        archive.mkdir(name, err => err ? reject(err) : resolve())
-      } else {
-        archive.writeFile(name, 'content', err => err ? reject(err) : resolve())
-      }
-    })
-  ).then(() => archive)
+  const folders = names.filter(name => isFolder(name))
+  const files = names.filter(name => !isFolder(name))
+
+  return bb.map(folders, mkdir, {concurrency: 50})
+    .then(() => bb.map(files, writeFile, {concurrency: 50}))
+    .then(() => archive)
 }
